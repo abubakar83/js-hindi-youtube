a441b3b14c2e4b00adb89b7175e0ced93b4ca7d9
Abu Bakar <110601493+abubakar83@users.noreply.github.com>
2025-05-10 03:50:44 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we can't use alert in node.js 
+
+let name = "hitesh"
+let age = 23
+let isLoggedIn = true
+
+// Permitive Data Types
+// numbers => 2 to power 53 
+// bigint = 
+// string = ""
+// boolean => true/false
+// null = standalone value
+// undefined => 
+// sumbol => unique 
+
+console.log(typeof null) // object 
+console.log(typeof undefined) //undefined
