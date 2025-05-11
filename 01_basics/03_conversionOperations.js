9501c3f05180191eb3cd61b8a5c0a3afab5fca7c
Abu Bakar <110601493+abubakar83@users.noreply.github.com>
2025-05-11 12:11:36 +0000

    conversion of data type

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,41 @@
+let score = 33 //"33", "33abc", "undefined", null
+
+// console.log(typeof score)
+// console.log(typeof(score)) 
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber) 
+// console.log(valueInNumber);
+
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true = 1; false = 0
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); // ture
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); //false
+// 1 => true, 0 => false
+// "" => false
+// "hitesh" true;
+
+let someNumber = 33
+let stringNumber = String(33)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+// let abc = ''
+// let abcNew = Number(abc)
+// console.log(abcNew)
+// console.log(typeof abcNew)
+
+// browser gives alot of values in string that's why we need coversion
+
