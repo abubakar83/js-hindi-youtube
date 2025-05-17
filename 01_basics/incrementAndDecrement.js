ecd5466c094d9a97bda4a0e325cdf020bf946004
Abu Bakar <110601493+abubakar83@users.noreply.github.com>
2025-05-17 11:21:35 +0000

    Increment And Decrement

---
+++ 01_basics/incrementAndDecrement.js
@@ -0,0 +1,18 @@
+// Increment and Decrement operatoer in Js 
+// Prefix 
+let x = 3 
+let y = ++x 
+
+console.log('x', x, "y", y )
+
+let a, b;
+a = b = 3
+b = ++a;
+console.log(a, b);
+
+// Prefix 
+let c = 3n;
+let d = c++
+console.log(c, d); // c = 4n, d = 3n
+
+ 
