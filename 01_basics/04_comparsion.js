83e877fce12a6ff5bfaa9f5f3968851898544ece
Abu Bakar <110601493+abubakar83@users.noreply.github.com>
2025-05-25 01:37:03 +0000

    comparsion operators

---
+++ 01_basics/04_comparsion.js
@@ -0,0 +1,20 @@
+console.log(2 > 1)
+console.log(2 < 1)
+console.log(2 >= 1)
+console.log(2 <= 1)
+console.log(2 == 1)
+console.log(2 != 1)
+
+console.log("2" > 1) //true
+console.log("02" > 1) // true
+
+console.log(null > 0) //false
+console.log(null == 0) // false
+console.log(null >= 0) // true (becuase comparsion and equal operator work differently)
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+// ===
+console.log("2" === 0)
