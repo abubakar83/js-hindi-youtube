ec4fcaa142b8dd8d9ebf3b3bf42fedb85608292f
Abu Bakar <110601493+abubakar83@users.noreply.github.com>
2025-06-27 05:17:46 +0000

    exmple of scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -8,9 +8,43 @@ let a = 300
 if(true) {
     let a = 10
     const b = 20
-    console.log("INNER: ", a)
+    // console.log("INNER: ", a)
 }
 
-console.log(a)
-console.log(b)
-console.log(c)
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+function one () {
+    const name = "abubakar"
+    function two () {
+        const website = "youtube"
+        console.log(name)
+    }
+    // console.log(website) // it will not access 
+    two()
+}
+// one()
+
+if(true) {
+    const name = "Hitesh"
+    if(name === "Hitesh"){
+        const website = " youtube"
+        // console.log(name + website)
+    }
+    // console.log(website) it will not work
+}
+// console.log(name) it will not work 
+
+// _+++++++++++++++++++++++++++++++++++INTRESTING++++++++++++++++++++++++++++++
+// execution before and after
+console.log(one(5))
+
+function one (num) {
+    return num +1
+}
+
+const addTwo = function (num) {
+    return num + 2
+}
+addTwo(5)
