e2f5d302875ee9b846f445522966c3206b752ecf
Abu Bakar <110601493+abubakar83@users.noreply.github.com>
2025-06-29 16:08:17 +0000

    js ececutioin and call stack

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -8,3 +8,25 @@
 // ( () => console.log(`DB CONNECTED`))()
 
 ((name) => console.log(`${name}`))("hell0")
+
+
+// javascript execuation context and call stack
+// there are two types of execution context.    
+// 1. global execution context => (node have own window have own) this key word
+// 2. function or functional execution context 
+// Execution Procudure,
+// first memory creation phase or creation phase (memory allocatin)
+// 
+// global execuation => this => memory phase => 
+// val1 => undefined
+// val2 => undefined
+// addnum => defination 
+// result1 = undefined
+// result2 = undefined
+// memory phase => execution phase =>
+// val1 = 10
+// val2 = 5
+// result1 = addNum => new variable enviroment  + execution thread 
+
+// and 3 is EVAL execuation context but in mangoose
+// javascript is single thread 
