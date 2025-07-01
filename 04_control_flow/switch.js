76861990cfe52afc73dc51e015bbbc5fc887a210
Abu Bakar <110601493+abubakar83@users.noreply.github.com>
2025-07-01 15:13:48 +0000

    basic of switch statement

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// control statement 
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log(`january`);
+        break; 
+    case 2:
+        console.log(`feb`);
+        break; 
+    case 3:
+        console.log(`march`);
+        break;
+    case 4:
+        console.log(`april`);
+        break;
+
+        default: 
+        console.log(`default case match`)
+}
+// if break key word not present in code then after matching the code all code excuted except default case
